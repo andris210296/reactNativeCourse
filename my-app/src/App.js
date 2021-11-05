a7422a4eba57cf5e6d1a6e5ee9ac197aad087c77
Andris Andermann <andris.andermann@terra.com.br>
2021-11-04 23:43:07 -0300

    Change of state
    Everytime the array is change the page exhibits the new goal. The input text is also transformed in empty

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import GoalList from './components/GoalList/GoalList';
 import NewGoal from './components/NewGoal/NewGoal';
@@ -6,23 +6,23 @@ import './App.css';
 
 const App = () => {
 
-  const courseGoals = [
-    {id:'cg1', text:'Finish the Course'},
-    {id:'cg2', text:'Learn all about the Course Main Topic'},
-    {id:'cg3', text:'Help other students in the Course Q&A'}
-  ];
+  const [courseGoals, setCourseGoals] = useState([
+    { id: 'cg1', text: 'Finish the Course' },
+    { id: 'cg2', text: 'Learn all about the Course Main Topic' },
+    { id: 'cg3', text: 'Help other students in the Course Q&A' }
+  ]);
 
-  const addNewGoalHandler = (newGoal) => {
-    courseGoals.push(newGoal);
-    console.log(courseGoals);
-  }
+  const addNewGoalHandler = newGoal => {
+    //setCourseGoals(courseGoals.concat(newGoal));
+    setCourseGoals(prevCourseGoals => prevCourseGoals.concat(newGoal));
+  };
 
 
   return (
     <div className="course-goals">
       <h2> Course Goals</h2>
-      <NewGoal onAddGoal={addNewGoalHandler}/>
-      <GoalList goals={courseGoals}/>
+      <NewGoal onAddGoal={addNewGoalHandler} />
+      <GoalList goals={courseGoals} />
     </div>
   );
 };
