7203294e32aa65a8f4cf8d79fb83d507b39d6e2c
Andris Andermann <andris.andermann@terra.com.br>
2021-11-04 23:12:18 -0300

    Adding new goals
    This commit add a ne goal every time someone press a button, however the change can only be seeing on console log. The next commit will support to change the page every time someone adds a new goal.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import GoalList from './components/GoalList';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
 import './App.css';
 
 const App = () => {
@@ -8,12 +9,19 @@ const App = () => {
   const courseGoals = [
     {id:'cg1', text:'Finish the Course'},
     {id:'cg2', text:'Learn all about the Course Main Topic'},
-    {id:'cg3', text:'Help other students in the Course Q&A'}  ];
+    {id:'cg3', text:'Help other students in the Course Q&A'}
+  ];
+
+  const addNewGoalHandler = (newGoal) => {
+    courseGoals.push(newGoal);
+    console.log(courseGoals);
+  }
 
 
   return (
     <div className="course-goals">
       <h2> Course Goals</h2>
+      <NewGoal onAddGoal={addNewGoalHandler}/>
       <GoalList goals={courseGoals}/>
     </div>
   );
