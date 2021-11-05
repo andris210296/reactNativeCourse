e7ce8d209fd7dd47a3295db6dc3e5173de96bf76
Andris Andermann <andris.andermann@terra.com.br>
2021-11-04 21:56:02 -0300

    Another way of doing components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+
 const App = () => {
-  return <h1> A React App!</h1>;
+  return <h1> A React App!</h1>;// => React.creatElement('h1',{},...)
 };
+
+
+/*
+Another way to do it
+
+class App extends React.Component {
+  render (){
+    return <h1> A React App!</h1>;
+  }
+}
+*/
 
 export default App;
