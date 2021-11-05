d8fb3362f63654380567a7a9e757a436175f3702
Andris Andermann <andris.andermann@terra.com.br>
2021-11-04 22:16:29 -0300

    Components creation
    This commit shows how to create components with React.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
 
+import GoalList from './components/GoalList';
+import './App.css';
 
 const App = () => {
-  return <h1> A React App!</h1>;// => React.creatElement('h1',{},...)
+  return (
+    <div className="course-goals">
+      <h2> Course Goals</h2>
+      <GoalList />
+    </div>
+  );
 };
 
-
-/*
-Another way to do it
-
-class App extends React.Component {
-  render (){
-    return <h1> A React App!</h1>;
-  }
-}
-*/
 
 export default App;
