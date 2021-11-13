f103ac1a5954dce7bcd0eec4d78fe48d53e00b5b
Andris Andermann <andris.andermann@terra.com.br>
2021-11-13 16:29:25 -0300

    Use of ScrollView

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
 
@@ -26,13 +26,13 @@ export default function App() {
         <Button title="ADD" onPress={addGoalHandler} />
 
       </View>
-      <View>
+      <ScrollView>
         {courseGoals.map((goal) => 
         <View key={goal} style={styles.listItem}>
           <Text>{goal}</Text>
         </View>
         )}
-      </View>
+      </ScrollView>
     </View>
   );
 }
