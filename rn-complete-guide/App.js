20ae1905405471bfaf7da11105e5b71d2a3996cc
Andris Andermann <andris.andermann@terra.com.br>
2021-11-13 16:22:51 -0300

    Update App.js
    This commit contains the code that adds dynamically into a list every item added on text input.

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.input} />
-        <Button title="ADD" />
+        <TextInput placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
 
       </View>
       <View>
-
+        {courseGoals.map((goal) => 
+        <View key={goal} style={styles.listItem}>
+          <Text>{goal}</Text>
+        </View>
+        )}
       </View>
     </View>
   );
@@ -32,6 +53,15 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     padding: 10
+  },
+
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+
   }
 
 });
