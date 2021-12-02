ccde97429bd016116fa8edb29cc33a837059f864
Andris Andermann <andris.andermann@terra.com.br>
2021-12-02 17:16:26 -0300

    Styling list of guesses
    This commit shows how to use flexGrow and contentContainerStyle

--- guess-number/screens/GameScreen.js
+++ guess-number/screens/GameScreen.js
@@ -6,6 +6,7 @@ import NumberContainer from '../components/NuberContainer';
 import Card from '../components/Card';
 import DefaulStyles from '../constants/default-styles';
 import MainButton from '../components/MainButton';
+import BodyText from '../components/BodyText';
 
 const generateRandomBetween = (min, max, exclude) => {
     min = Math.ceil(min);
@@ -19,6 +20,13 @@ const generateRandomBetween = (min, max, exclude) => {
     }
 
 };
+
+const renderListItem = (value, numOfRounds) => (
+    <View key={value} style={styles.listItem}>
+        <BodyText>#{numOfRounds}</BodyText>
+        <BodyText>{value}</BodyText>
+    </View>
+);
 
 const GameScreen = props => {
     const initialGuess = generateRandomBetween(1, 100, props.userChoice);
@@ -69,13 +77,11 @@ const GameScreen = props => {
                     <Ionicons name="md-add" size={24} color="white" />
                 </MainButton>
             </Card>
-            <ScrollView>
-                {pastGuesses.map(guess =>
-                    <View key={guess}>
-                        <Text>{guess}</Text>
-                    </View>
-                )}
-            </ScrollView>
+            <View style={styles.listContainer}>
+                <ScrollView contentContainerStyle={styles.list}>
+                    {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
+                </ScrollView>
+            </View>
 
         </View>
     );
@@ -94,6 +100,25 @@ const styles = StyleSheet.create({
         marginTop: 20,
         width: 400,
         maxWidth: '90%'
+    },
+    listContainer: {
+        flex: 1,
+        width: '80%'
+    },
+    list: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent:'flex-end'
+    },
+    listItem: {
+        borderColor: '#ccc',
+        borderWidth: 1,
+        padding: 15,
+        marginVertical: 10,
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '60%'
     }
 });
 
