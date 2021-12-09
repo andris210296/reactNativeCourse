02b1be372da5a71702ac0ecee62025a91d85da44
Andris Andermann <andris.andermann@terra.com.br>
2021-12-09 20:14:26 -0300

    Working with smaller screens
    This commit resizes the app according to the width and height of the phone' screen. Also contains the use of Dimensions in "if"checks

--- guess-number/screens/StartGameScreen.js
+++ guess-number/screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard, Alert, Dimensions } from 'react-native';
 
 import Colors from '../constants/colors';
 import Card from '../components/Card';
@@ -90,8 +90,10 @@ const styles = StyleSheet.create({
         fontFamily: 'open-sans-bold'
     },
     inputContainer: {
-        width: 300,
-        maxWidth: '80%',
+        width: '80%',
+        //maxWidth: '80%',
+        maxWidth: '95%',
+        minWidth: 300,
         alignItems: 'center'
     },
     buttonContainer: {
@@ -101,7 +103,8 @@ const styles = StyleSheet.create({
         paddingHorizontal: 15
     },
     button: {
-        width: 100
+        //width: 100
+        width: Dimensions.get('window').width / 4
     },
     input: {
         width: 50,
